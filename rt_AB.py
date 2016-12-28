ff2102af76959e8ab6149d5cdf70dbb5e0242835
Andres Kütt <andres.kytt@gmail.com>
2016-12-28 19:34:29 +0200

    Major linting

--- rt_AB.py
+++ rt_AB.py
@@ -8,9 +8,11 @@ import sys
 import datetime
 
 start = 0
+
+
 # Print iterations progress
-def printProgress (iteration, total, prefix = '', suffix = '', decimals = 1, barLength = 100, ETA = True):
-    """ 
+def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100, eta=True):
+    """
     Call in a loop to create terminal progress bar
     @params:
         iteration   - Required  : current iteration (Int)
@@ -21,69 +23,71 @@ def printProgress (iteration, total, prefix = '', suffix = '', decimals = 1, bar
         barLength   - Optional  : character length of bar (Int)
     """
     global start
-    start = datetime.datetime.now() if start == 0 else start 
-    formatStr       = "{0:." + str(decimals) + "f}"
-    percents        = formatStr.format(100 * (iteration / float(total)))
-    filledLength    = int(round(barLength * iteration / float(total)))
-    bar             = '█' * filledLength + '-' * (barLength - filledLength)
-    eta             = 'ETA ' + (start + (datetime.datetime.now() - start)*total/iteration).strftime('%H:%M:%S') if ETA and iteration > 0 else ''
+    start = datetime.datetime.now() if start == 0 else start
+    format_str = "{0:." + str(decimals) + "f}"
+    percents = format_str.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+    eta_ts = (start + (datetime.datetime.now() - start) * total / iteration) if iteration > 0 else 0
+    eta = 'ETA ' + eta_ts.strftime('%H:%M:%S') if eta and iteration > 0 else ''
     sys.stdout.write('\r%s |%s| %s%s %s %s' % (prefix, bar, percents, '%', suffix, eta)),
     if iteration == total:
         sys.stdout.write('\n')
     sys.stdout.flush()
 
-def getComplexity(fname):
-    r = etree.parse(fname).getroot()
-    ns = namespace(r)
-    #return r.xpath("count(//sisuTekst)")
-    return len(r.findall('.//{0}sisuTekst'.format(ns)))
+
+def get_complexity(fname):
+    cr = etree.parse(fname).getroot()
+    ns = namespace(cr)
+    return len(cr.findall('.//{0}sisuTekst'.format(ns)))
+
 
 def namespace(element):
-    m = re.match('\{.*\}', element.tag)
-    return m.group(0) if m else ''
+    ns_m = re.match('\{.*\}', element.tag)
+    return ns_m.group(0) if m else ''
 
-def getMatch(fname, act):
-    r = etree.parse(fname).getroot()
-    ns = namespace(r)
-    t = r.find('.//{0}pealkiri'.format(ns)).text
-    #return (act, re.compile('(.*)(' + t +'\w*(\s|\Z)+)', re.I))
-    return(act, re.compile(t + '\w*?', re.I))
+
+def get_match(fname):
+    m_r = etree.parse(fname).getroot()
+    ns = namespace(m_r)
+    t = m_r.find('.//{0}pealkiri'.format(ns)).text
+    return re.compile(t + '\w*?', re.I)
+
 
 def calculate(task):
-    a, s, t = task
-    #print("calculate " + a)
-    return (a, len(re.findall(s, t)))
+    ca, cs, t = task
+    return ca, len(re.findall(cs, t))
 
-def getLinks(fname, act, searchList):
-    r = etree.parse(fname).getroot()
-    ns = namespace(r)
+
+def get_links(fname, s_list):
+    l_r = etree.parse(fname).getroot()
+    ns = namespace(l_r)
     txt = ""
 
-    #print("finding children")
-    for child in r.findall('.//{0}tavatekst'.format(ns)):
+    for child in l_r.findall('.//{0}tavatekst'.format(ns)):
         if child.text:
             txt = txt + "\t" + child.text
 
     PROCESSES = 8
-    TASKS = [(a, s, txt) for (a, s) in searchList]
+    TASKS = [(t_act, t_str, txt) for (t_act, t_str) in s_list]
     
     matches = dict()
     with multiprocessing.Pool(PROCESSES) as pool:
-        for a, c in pool.map(calculate, TASKS):
-            matches[a] = c
+        for ma, c in pool.map(calculate, TASKS):
+            matches[ma] = c
 
     return matches
 
 # Get the titles of acts per month and compile a list of REs to match them
-r = re.compile("(\d+)\.(\d+)\.(\d+)\-(.*)\.xml", re.I)
+r = re.compile("(\d+)\.(\d+)\.(\d+)-(.*)\.xml", re.I)
 d = dict() 
 bCount = dict()
 l = os.listdir('.')
 i = 0
 print('Extract titles and complexities')
 for filename in l:
-    printProgress(i, len(l), prefix = 'Progress:', suffix = 'Complete', barLength = 50)
-    i = i + 1
+    print_progress(i, len(l), prefix='Progress:', suffix='Complete', barLength=50)
+    i += 1
 
     m = re.match(r, filename)
     if m:
@@ -92,9 +96,9 @@ for filename in l:
             d[k] = []
             bCount[k] = dict() 
 
-        bCount[k][m.group(4)] = getComplexity(filename) 
+        bCount[k][m.group(4)] = get_complexity(filename)
 
-        d[k].append(getMatch(filename, m.group(4)))
+        d[k].append(get_match(filename))
 
 print('\nTitle extraction done')
 
@@ -103,8 +107,8 @@ print('Starting to find links')
 
 i = 0
 for filename in os.listdir('.'):
-    printProgress(i, len(l), prefix = 'Progress:', suffix = 'Complete', barLength = 50)
-    i = i + 1
+    print_progress(i, len(l), prefix ='Progress:', suffix ='Complete', barLength = 50)
+    i += 1
 
     m = re.match(r, filename)
     if m:
@@ -113,11 +117,10 @@ for filename in os.listdir('.'):
         continue
 
     act = m.group(4)
-    if not k in matrices:
+    if k not in matrices:
         matrices[k] = dict()
 
-    matrices[k][act] = getLinks(filename, act, d[k])
-    #print(matrices[k][act])
+    matrices[k][act] = get_links(filename, d[k])
 
 print('\nLinks done')
 
